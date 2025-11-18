36b19a96edcb0c1ed3aa0bc5efb8c55b35bd50e4
LiebPR <jplatero773@gmail.com>
2025-11-18 12:48:46 +0100

    PlayerController

---
+++ BurdenOfChoices_Project/Assets/_BurdenOfChoices_Root/Scripts/Player/PlayerController.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+/// <summary>
+/// PlayerController: Contiene las logicas de movimiento del jugador y la de agacharse
+/// </summary>
+public class PlayerController : MonoBehaviour
+{
+    #region General Variables
+    [Header("Movement")]
+
+    [Header("Chase")]
+    #endregion
+}
